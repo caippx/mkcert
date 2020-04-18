65b5e10986e13aea390ad759a494103a6d6ecb47
caippx <37325838+caippx@users.noreply.github.com>
2020-04-18 13:24:21 +0800

    Update cert.go

--- cert.go
+++ cert.go
@@ -306,11 +306,9 @@ func (m *mkcert) newCA() {
 
 	tpl := &x509.Certificate{
 		SerialNumber: randomSerialNumber(),
-		domain := strings.Replace(hosts[0], ":", "_", -1)
-		domain = strings.Replace(defaultName, "*", "_wildcard", -1)
 		Subject: pkix.Name{
 			Organization:       []string{"PPX CA"},
-			OrganizationalUnit: []string{domain},
+			OrganizationalUnit: []string{defaultName},
 
 			// The CommonName is required by iOS to show the certificate in the
 			// "Certificate Trust Settings" menu.
